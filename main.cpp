6f2ae907e0b9985356ec6460afb9bf3ae0e7adf3
pon-droid <tellepsarecool@gmail.com>
2022-10-04 20:33:59 +1100

    Rudimentary let statement parsing

--- main.cpp
+++ main.cpp
@@ -11,13 +11,24 @@ public:
 
 struct Ident { token ident_tok; std::string val; };
 
+
+/*
 class LetStatement {
 public:
   token let_token;
   std::string val;
   std::string print_token() const { return let_token.data; }
 };
+*/
 
+class LetStatement {
+public:
+  token let;
+  token ident;
+  std::vector<token> expression;
+  explicit LetStatement(const token& let, const token& ident): let(let), ident(ident) {}
+};
+  
 class ReturnStatement {
 public:
   token ret_token;
@@ -38,6 +49,7 @@ int main(int argc, char**argv){
   lexer.read_in();
 
   std::vector<LetStatement> statements;
+  /*
   for(size_t i = 0; i < lexer.tokens.size(); i++){
     switch(lexer.tokens[i].type){
     case LET:
@@ -65,7 +77,53 @@ int main(int argc, char**argv){
   for(const auto &i: statements){
     std::cout << i.val << "\n";
   }
-  
+  */
+
+#define TOKTYPE(index) (lexer.tokens[index].type)
+#define TOK(index)  (lexer.tokens[index])
+
+  for(size_t i = 0; i < lexer.tokens.size(); i++){
+    switch(TOKTYPE(i)){
+    case LET:{
+      
+      if(TOKTYPE(i+1) != IDENT){
+	std::cout << "parser fail: expected next token to be identifier, got " <<
+	TOK(i+1).data << " of type " << TOKTYPE(i+1) << "\n";
+        continue;
+      }
+      else if (TOKTYPE(i+2) != ASSIGN){	
+	std::cout << "parser fail: expected next token to be assignment = , got " <<
+	TOK(i+2).data << " of type " << TOKTYPE(i+2) << "\n";
+	continue;
+      }
+      
+      assert(TOKTYPE(i+1) == IDENT && TOKTYPE(i+2) == ASSIGN);
+      
+      LetStatement let(TOK(i), TOK(i+1));
+      i+=3;
+      
+      for(;TOKTYPE(i) != SEMICOLON; i++){
+	let.expression.push_back(TOK(i));
+      }
+      
+      statements.push_back(let);
+    }
+      
+    default: continue;
+      
+    }
+  }
+
+
+  for(const auto &i: statements){
+    std::cout << i.ident.data << " | ";
+    for(const auto &j: i.expression){
+      std::cout << j.data << " ";
+    };
+    std::cout << "\n";
+  }
+      
+      
 
 
   
